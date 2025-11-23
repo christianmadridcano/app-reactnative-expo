ad7b659f87be25078ca64c97fc8466aa9ff5ebf0
christianmadridcano <christianmadridcano@gmail.com>
2025-11-23 10:18:21 -0300

    Limpie codigo innecesario en app/(tabs)/index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,8 @@ import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
-  const [count, setCount] = useState(0);
-  const [open, setOpen] = useState(false);
   const { user, logout } = useAuth();
   const router = useRouter();
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  }
-
-  const handleToggle = () => {
-    setOpen(!open);
-  }
 
   const handleLogout = () => {
     logout();
@@ -25,18 +15,9 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Bienvenido {user?.name}</Text>
-      <Text>This is a counter: {count}</Text>
-      <Pressable style={styles.button} onPress={handleIncrement}>
-        <Text style={styles.buttonText}>Increment</Text>
-      </Pressable>
-      <Text>The toggle is {open ? 'ON' : 'OFF'}</Text>
-      <Pressable style={styles.button} onPress={handleToggle}>
-        <Text style={styles.buttonText}>Toggle</Text>
-      </Pressable>
+      
+     
       <Text>Hola {user?.name}</Text>
-      <Link href="/modal" style={styles.button}>
-        <Text style={styles.buttonText}>Open Modal</Text>
-      </Link>
 
       <Pressable style={styles.button} onPress={handleLogout}>
         <Text style={styles.buttonText}>Logout</Text>
